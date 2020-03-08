2a0850718eee97b187c40c9cfa8739db851f9c98
GuJunBing <>
2020-03-08 16:43:09 +0800

    update to test 0.0.3

--- SSAComponentGoodsDetailCategory/Classes/SSAComponentGoodsDetailCategory_Extension.swift
+++ SSAComponentGoodsDetailCategory/Classes/SSAComponentGoodsDetailCategory_Extension.swift
@@ -1,6 +1,6 @@
 //
-//  SSAComponentGoodsDetailCategory.swift
-//  Pods-SSAComponentGoodsDetailCategory_Example
+//  SSAComponentGoodsDetailCategory_Extension.swift
+//  SSAComponentGoodsDetailCategory
 //
 //  Created by GuJunBing on 2020/3/8.
 //
@@ -8,7 +8,7 @@
 import CTMediator
 
 public extension CTMediator {
-    @objc func goToGoodsDetailsVC4Swift(callback:@escaping (String) -> Void,goodsId: String!,goodsName: String!) -> UIViewController? {
+    @objc func goToGoodsDetailsVC4Swift(_ callback:@escaping (String) -> Void,goodsId: String!,goodsName: String!) -> UIViewController? {
         let params = [
             "callback":callback,
             "goodsId":goodsId,
@@ -21,7 +21,7 @@ public extension CTMediator {
         return nil
     }
     
-    @objc func goToGoodsDetailsVC4OC(callback:@escaping (String) -> Void,goodsId: String!,goodsName: String!) -> UIViewController? {
+    @objc func goToGoodsDetailsVC4OC(_ callback:@escaping (String) -> Void,goodsId: String!,goodsName: String!) -> UIViewController? {
         let callbackBlock = callback as @convention(block) (String) -> Void
         let callbackBlockObject = unsafeBitCast(callbackBlock, to: AnyObject.self)
         let params = ["callback":callbackBlockObject, "goodsId":goodsId,
@@ -32,4 +32,5 @@ public extension CTMediator {
         return nil
     }
 }
+
 
